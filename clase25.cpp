3a292d1617cc1d7d82deef677daf1ba28c182593
diego moreno <da.moreno17@uniandes.edu.co>
2019-11-05 18:49:16 +0000

    primera parte, numero de entrada parte 2

--- clase25.cpp
+++ clase25.cpp
@@ -2,7 +2,7 @@
 #include<cmath>
 using namespace std;
 void llenado(float **a, int filas, int columnas);
-void imp(float *a,int filas,int columnas);
+void imp_head(float *a,int filas,int columnas);
 int main(){
   int numero;
   cout<<"Escriba un numero entre el 3 y el 11 "<<endl;
@@ -10,4 +10,35 @@ int main(){
   if(numero<3 || numero>11){
     cout<<"el numero está mal, verifiquelo"<<endl;
   }
+/*inicializacion de la matriz*/
+  float **matriz;
+  matriz = new float *[numero+2];
+  for(int i = 0; i<numero+2;i++)
+   matriz[i] = new float[numero+2];
+  llenado(matriz,numero+2,numero);
+  imp_head(*[numero+2],numero+2,numero);
+  return 0; 
+}
+
+void llenado(float **a, int filas, int columnas){
+  for (int i = 0; i<filas; i++){
+    for(int j = 0; j<columnas;j++){
+      a[i][j] = i+j;
+    }
+
+  }
+
+}
+void imp_head(float *a,int filas,int columnas){
+  for(int i = 0; i<columnas; i++){
+    cout << "la cabeza es"<<i<<":"<<a[i]<<endl;
+  }
+  float sum = 0;
+  for(int i = 0;i<filas;i++){
+    int pseudo_sum = columnas +i;
+    sum = sum + *(a + pseudo_sum);
+  }
+  cout<<"resultado de sumar columna 1"<<sum<<endl;
+    
+    
 }
