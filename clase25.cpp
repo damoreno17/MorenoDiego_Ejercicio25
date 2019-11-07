b5af293c518e4c0c6c84831e99cb004050d596cd
diego moreno <da.moreno17@uniandes.edu.co>
2019-11-07 01:58:53 +0000

    correccion ejercicio 25 en casa

--- clase25.cpp
+++ clase25.cpp
@@ -1,44 +1,60 @@
 #include<iostream>
 #include<cmath>
+
 using namespace std;
-void llenado(float **a, int filas, int columnas);
-void imp_head(float *a,int filas,int columnas);
+
+float** llenado(float** a, int filas, int columnas);
+int imp_head(float** a,int filas,int columnas);
+
+
 int main(){
   int numero;
+    
   cout<<"Escriba un numero entre el 3 y el 11 "<<endl;
+    
   cin>>numero;
-  if(numero<3 || numero>11){
+    
+  if(numero<3 || numero>11)
+  {
+      
     cout<<"el numero está mal, verifiquelo"<<endl;
   }
+    
 /*inicializacion de la matriz*/
   float **matriz;
-  matriz = new float *[numero+2];
-  for(int i = 0; i<numero+2;i++)
-   matriz[i] = new float[numero+2];
+    
+  matriz = new float* [numero+2];
+    
+
   llenado(matriz,numero+2,numero);
-  imp_head(*[numero+2],numero+2,numero);
+  imp_head(matriz,numero+2,numero);
+  delete matriz;
+
+    
   return 0; 
 }
 
-void llenado(float **a, int filas, int columnas){
+float** llenado(float **a, int filas, int columnas){
   for (int i = 0; i<filas; i++){
+      a[i] = new float [columnas];
     for(int j = 0; j<columnas;j++){
       a[i][j] = i+j;
+      cout<<a[i][j]<<endl;
     }
 
   }
+  return a;
 
 }
-void imp_head(float *a,int filas,int columnas){
+int imp_head(float **a,int filas,int columnas){
+    int counter = 0;
   for(int i = 0; i<columnas; i++){
-    cout << "la cabeza es"<<i<<":"<<a[i]<<endl;
+    cout << "cada fila da = "<<*a[i]<<endl;
   }
-  float sum = 0;
   for(int i = 0;i<filas;i++){
-    int pseudo_sum = columnas +i;
-    sum = sum + *(a + pseudo_sum);
+    counter += a[0][i];
   }
-  cout<<"resultado de sumar columna 1"<<sum<<endl;
+  cout<<"el resultado final de sumar las columnas es"<<counter<<endl;
     
     
 }
